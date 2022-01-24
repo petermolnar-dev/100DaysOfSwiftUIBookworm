16c4b9b0105264d62062a9e943bf1ba035114ca0
Peter Molnar <petermolnar.hu@gmail.com>
2022-01-24 18:38:39 +0100

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Peter Molnar on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
