c00c6d963aece68db1c7d06e722c47c7b2ecface
Peter Molnar <petermolnar.hu@gmail.com>
2022-01-30 16:36:20 +0100

    Adding Delete book and confirmation alert to the Detailed book view.

--- Bookworm/DetailView.swift
+++ Bookworm/DetailView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 import CoreData
 
 struct DetailView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    @State private var showingDeleteAlert = false
+    
     let book: Book
+    
     var body: some View {
         ScrollView {
             ZStack(alignment: .bottomTrailing) {
@@ -38,7 +43,29 @@ struct DetailView: View {
         }
         .navigationTitle(book.title ?? "Unknown Book")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Delete book", isPresented: $showingDeleteAlert) {
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Are you sure?")
+        }
+        .toolbar {
+            Button {
+                showingDeleteAlert = true
+            } label: {
+                Label("Delete this book", systemImage: "trash")
+            }
+        }
     }
+    
+    func deleteBook() {
+        moc.delete(book)
+        
+        try? moc.save()
+        
+        dismiss()
+    }
+    
 }
 
 struct DetailView_Previews: PreviewProvider {
